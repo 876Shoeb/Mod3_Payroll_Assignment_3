4e5ddbeaa5d5fe205f8f5ac90748e6809cc616cc
NicolaGray <nickkigray30@gmail.com>
2022-03-30 23:26:14 -0500

    Base_Plus_CommissionEmployee Class

--- Base_Plus_CommissionEmployee.java
+++ Base_Plus_CommissionEmployee.java
@@ -5,5 +5,27 @@ public class Base_Plus_CommissionEmployee {
                                         double grossSales, double commissionRate, double baseSalary)
     {
         super(firstName, lastName,socialSecurityNumber, grossSales, commissionRate);
+
+        if(baseSalary < 0.0)
+            throw new IllegalArgumentException("base salary must be >=0.0");
+
+        this.baseSalary = baseSalary;
+
+    }
+    public void setBaseSalary(double baseSalary)
+    {
+        if(baseSalary<0.0)
+            throw new IllegalArgumentException("base salary must be >=0.0");
+
+        this.baseSalary = baseSalary;
+    }
+    public double getBaseSalary()
+    {
+        return getBaseSalary() + super.earnings();
+    }
+    @Override
+    public String toString()
+    {
+        return String.format("%s, %s; %s; $%,.2f", "base-salaried", super.toString(), "base salary", getBaseSalary());
     }
 }
