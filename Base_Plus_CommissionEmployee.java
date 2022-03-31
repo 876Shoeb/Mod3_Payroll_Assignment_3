76c45bb9cb518e527ceb09fc02946ea85466a82e
NicolaGray <nickkigray30@gmail.com>
2022-03-30 22:51:48 -0500

    Base_Plus_CommissionEmployee Class

---
+++ Base_Plus_CommissionEmployee.java
@@ -0,0 +1,9 @@
+public class Base_Plus_CommissionEmployee {
+    private double baseSalary;
+
+    public Base_Plus_CommissionEmployee(string firstName, string lastName, string socialSecurityNumber,
+                                        double grossSales, double commissionRate, double baseSalary)
+    {
+        super(firstName, lastName,socialSecurityNumber, grossSales, commissionRate);
+    }
+}
