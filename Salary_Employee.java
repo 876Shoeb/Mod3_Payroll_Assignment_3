93068d36f081e4467a47741198e6196683503b0d
876Shoeb <78888023+876Shoeb@users.noreply.github.com>
2022-03-30 22:11:20 -0500

    edit

--- Salary_Employee.java
+++ Salary_Employee.java
@@ -1,9 +1,9 @@
 package com.XTi.Projects;
 
-public abstract class Salary_Employee extends com.XTi.Projects.Employee {
+public abstract class SalariedEmployee extends com.XTi.Projects.Employee {
     private double WeeklySalary;
 
-    public Salary_Employee(String firstname, String lastname, String socialsecuritynum, double weeklySalary) {
+    public SalariedEmployee(String firstname, String lastname, String socialsecuritynum, double weeklySalary) {
         super(firstname, lastname, socialsecuritynum);
         if (WeeklySalary < 0.0)
             throw new IllegalArgumentException("Weekly Salary Should Be <= 0.0");
@@ -28,7 +28,7 @@ public abstract class Salary_Employee extends com.XTi.Projects.Employee {
     @Override
     public String toString()
     {
-        return String.format("Salary_Employee: %s%n%s: $%, .2f",super.toString(), "Weekly Salary", getWeeklySalary());
+        return String.format("Salaried Employee: %s%n%s: $%, .2f",super.toString(), "Weekly Salary", getWeeklySalary());
     }
 }
 
