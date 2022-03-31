9844344c16971a272b504c7613cd3ef3370606f7
876Shoeb <78888023+876Shoeb@users.noreply.github.com>
2022-03-30 20:50:19 -0500

    New Classes

---
+++ Employee.java
@@ -0,0 +1,31 @@
+package com.XTi.Projects;
+
+public abstract class Employee {
+    private String Firstname;
+    private String Lastname;
+    private String SocialSecurityNum;
+
+    public Employee(String firstname, String lastname, String socialsecuritynum) {
+        this.Firstname = firstname;
+        this.Lastname = lastname;
+        this.SocialSecurityNum = socialsecuritynum;
+    }
+
+    public String getFirstname() {
+        return this.Firstname;
+    }
+
+    public String getLastname() {
+        return this.Lastname;
+    }
+
+    public String getSocialSecurityNum() {
+        return this.SocialSecurityNum;
+    }
+
+    public String toString() {
+        return String.format("%s %s%nSocial Security Number: %s", this.getFirstname(), this.getLastname(), this.getSocialSecurityNum());
+    }
+
+    public abstract double earnings();
+}
